e7ac6758d538e740fe8183d91571a8c65ce4617f
Paweł Smolak <smolak.paul@gmail.com>
2018-03-30 13:52:41 +0200

    Add function for scrapping all communities in scrap module

--- ogapi/scrap.py
+++ ogapi/scrap.py
@@ -7,7 +7,6 @@ BASE = 'https://{}.ogame.gameforge.com'
 
 def communities():
     """Scrap community list from ogame website."""
-
     r = utils.get(BASE.format('pl'))
     soup = BeautifulSoup(r.text, 'html.parser')
     tags = soup.find(id="mmoList1").find_all('a')
@@ -18,13 +17,17 @@ def communities():
 
 def servers(community):
     """Scrap all servers for particular community from ogame website."""
-
     r = utils.get(BASE.format(community))
     soup = BeautifulSoup(r.text, 'html.parser')
     tags = soup.find(id="serverLogin").find_all('option')
     servers = [int(tag.get('value').split('.')[0][1:-3]) for tag in tags]
 
     return servers
+
+def all():
+    """Scraps servers for all communities at once in parallel"""
+    action = lambda c: (c, servers(c))
+    return utils.parallel(communities(), action)
 
 
 if __name__ == "__main__":
